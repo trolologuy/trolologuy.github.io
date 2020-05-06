cd1b0e1944ed89ebd7d8118243875d1ff21db94d
trolologuy <16886290+trolologuy@users.noreply.github.com>
2020-05-06 12:54:49 +0200

    v1.0.3 added telegram bot notification on build

--- telegram-notifier.sh
+++ telegram-notifier.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-if [ $1 == "success" ]
+echo "Travis test result"
+echo ${TRAVIS_TEST_RESULT}
+
+if [ "${TRAVIS_TEST_RESULT}" == 0 ]
 then
     echo "The build was a success"
     curl -s -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d chat_id="${CHAT_ID}" -d text=$'✅ Succesfull build by Travis-CI.\n  Build number: '"${TRAVIS_JOB_ID}"$'\n  Test results: '"${TRAVIS_TEST_RESULT}"''
-elif [ $1 == "failure" ]
+elif [ "${TRAVIS_TEST_RESULT}" == 1 ]
 then
     echo "The build was a failure"
     curl -s -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d chat_id="${CHAT_ID}" -d text=$'❌ Unsuccesfull build by Travis-CI.\n  Build number: '"${TRAVIS_JOB_ID}"$'\n  Test results: '"${TRAVIS_TEST_RESULT}"''
 else
     echo "The build status could not be retrieved"
+    echo ${TRAVIS_TEST_RESULT}
     curl -s -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d chat_id="${CHAT_ID}" -d text=$'⚠️ The state of the build made by Travis-CI could not be retrieved.\n  Build number: '"${TRAVIS_JOB_ID}"$'\n  Test results: '"${TRAVIS_TEST_RESULT}"''
 fi
